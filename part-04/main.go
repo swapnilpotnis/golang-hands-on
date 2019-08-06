212bf2d04dd8ea4ddd703fa56c7447968b2046ba
swapnilpotnis <potnis.swapnil@gmail.com>
2019-08-06 14:51:56 +0530

    Commit Till Part-04

--- part-04/main.go
+++ part-04/main.go
@@ -15,7 +15,32 @@ func main() {
 	// %v : value of the variable/string/array
 	fmt.Printf("i: %v \n", slice)
 	fmt.Printf("i: %v \n", slice[1:])
+	
+/*
+In simple words _ is an ignored value. Go doesn't permit unused local variables and throws a compilation error if you try to do so. So you trick compiler saying "please ignore this value" by placing a _
 
+For example:
+
+// Here range of os.Args returns index, value
+for index, arg := range os.Args[1:] {
+    fmt.Println(arg)
+}
+
+Here Go throws compilation error because you are not using index (un used local variable) at all in the loop. Now you should make index to _ to say compiler to ignore it.
+
+for _, arg := range os.Args[1:] {
+    fmt.Println(arg)
+}
+
+
+*/
+
+
+/*
+ The range form of the for loop iterates over a slice or map.
+
+When ranging over a slice, two values are returned for each iteration. The first is the index, and the second is a copy of the element at that index. 
+*/
 	for _, v := range slice {
 		fmt.Printf("%s, ", v)
 	}
@@ -24,3 +49,4 @@ func main() {
 		fmt.Printf("%s, ", v)
 	}
 }
+
